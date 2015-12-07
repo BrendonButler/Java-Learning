7abf714b4423d64b972f2ceb3703cadfbd8eeee7
Brendon Butler <bbutler452@gmail.com>
2015-12-07 15:24:28 -0500

    added IO

--- src/Test.java
+++ src/Test.java
@@ -5,7 +5,19 @@ public class Test {
     private static Console console = System.console();
 
     public static void main(String[] args) {
-        console.printf("Hey look, it works!");
+        console.printf("Hey look, it works!\n\n");
+
+		boolean emptyName = true;
+
+		String name;
+
+		do {
+			name = console.readLine("What is your name?\n> ");
+
+			if (name != null) emptyName = false;
+		} while (emptyName);
+
+		console.printf("Are you ready to exit, %s?", name);
 
 		System.exit(0);
     }
