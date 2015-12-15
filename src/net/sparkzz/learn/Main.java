6f2fb3f4a6121738d8b56cac6873fe31baadaa84
Brendon Butler <bbutler452@gmail.com>
2015-12-15 14:32:43 -0500

    Started "Local Development Environments" learning

---
+++ src/net/sparkzz/learn/Main.java
@@ -0,0 +1,11 @@
+package net.sparkzz.learn;
+
+/**
+ * Created by Brendon Butler on 12/15/2015.
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
